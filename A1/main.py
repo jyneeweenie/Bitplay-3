45be93e60756c7d7ec561cbc774849e5ddec0e1a
jyneeweenie <jynemarie1@gmail.com>
2025-04-15 11:42:53 +0300

    done

---
+++ A1/main.py
@@ -0,0 +1,14 @@
+# Program to swap 2 numers without using 3rd variale
+def swap1(a, b):
+    # code to swap 'a' and 'b'
+    a = a^b
+    b = a^b
+    a = a^b
+    print("After swapping: a = ",a,"b= ", b)
+def swap2(a, b):
+    a = (a & b) + (a | b)
+    b = a + (~b) + 1
+    a = a + (~b) + 1
+    print ("After swapping: a = ", a, "b= ", b)
+swap1(1,2)
+swap2(1,2)
